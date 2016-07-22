c49e604f87c572cf2f52a488ed66a3203986aaab
Parimal Jariwala <Parimal Jariwala>
2016-07-22 23:30:17 +0530

    Done

--- app/Repositories/MachineRepository.php
+++ app/Repositories/MachineRepository.php
@@ -11,13 +11,8 @@ class MachineRepository extends BaseRepository
      * @var array
      */
     protected $fieldSearchable = [
-<<<<<<< HEAD
         'machine_name' => 'like',
         'machine_type' => 'like',
-=======
-        'machine_name',
-        'machine_type'
->>>>>>> 1f6119bd87453c2dfaede669f7bf0f3935315463
     ];
 
     /**
